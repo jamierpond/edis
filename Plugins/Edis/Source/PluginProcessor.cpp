9364f06ffad89fc07cb9b2398cb93997658ac66a
Jamie Pond <jamiepond259@gmail.com>
2025-07-17 00:39:52 -0700

    wip

--- Plugins/Edis/Source/PluginProcessor.cpp
+++ Plugins/Edis/Source/PluginProcessor.cpp
@@ -114,7 +114,7 @@ void EdisAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
 
             for (auto i = 0; i < mainInputOutput.getNumSamples(); ++i) {
 
-                auto [y, smoothed_gain] = pond::rm_sidechain<float>({
+                channel[i] = pond::rm_sidechain<float>({
                     .channel = channel[i],
                     .sidechain = sidechain[i],
                     .amount = amount,
@@ -123,7 +123,8 @@ void EdisAudioProcessor::processBlock(juce::AudioBuffer<float>& buffer,
                     .release_alpha = release_alpha
                 });
 
-                prev_smoothed_gain[c_sz] = smoothed_gain;
+                prev_smoothed_gain[c_sz] = channel[i].second;
+                channel[i] = channel[i].first;
             }
         }
     }
